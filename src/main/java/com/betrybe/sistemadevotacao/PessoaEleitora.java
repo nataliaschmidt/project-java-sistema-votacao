0d741eb2041346d3474352dfedb73efa50b5768d
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-10-27 14:31:51 -0300

    Implementa a classe PessoaEleitora

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,38 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+/**
+ * The type Pessoa eleitora.
+ */
+public class PessoaEleitora extends Pessoa {
 
+  private String cpf;
+
+  /**
+   * Instantiates a new Pessoa eleitora.
+   *
+   * @param nome o nome da pessoa eleitora
+   * @param cpf  o cpf da pessoa eleitora
+   */
+  public PessoaEleitora(String nome, String cpf) {
+    this.nome = nome;
+    this.cpf = cpf;
+  }
+
+  /**
+   * Gets cpf.
+   *
+   * @return the cpf
+   */
+  public String getCpf() {
+    return cpf;
+  }
+
+  /**
+   * Sets cpf.
+   *
+   * @param cpf the cpf
+   */
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
