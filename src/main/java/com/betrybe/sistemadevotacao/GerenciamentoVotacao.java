7daa716275f065167dc8217008208c434191d29f
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-10-27 15:12:28 -0300

    Implementa alguns métodos e atributos da classe GerenciamentoVotacao

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,5 +1,43 @@
 package com.betrybe.sistemadevotacao;
 
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
 
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+
+  public ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<PessoaCandidata>();
+  private ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<PessoaEleitora>();
+
+  private ArrayList<String> cpfsComputados = new ArrayList<String>();
+
+  @Override
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    for (PessoaCandidata pessoaCandidata : this.pessoasCandidatas) {
+      if (pessoaCandidata.getNumero() == numero) {
+        System.out.println("Número da pessoa candidata já utilizado!");
+        return;
+      }
+    }
+    pessoasCandidatas.add(new PessoaCandidata(nome, numero));
+  }
+
+  @Override
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+    for (PessoaEleitora pessoaEleitora : this.pessoasEleitoras) {
+      if (pessoaEleitora.getCpf() == cpf) {
+        System.out.println("Pessoa eleitora já cadastrada!");
+        return;
+      }
+    }
+    pessoasEleitoras.add(new PessoaEleitora(nome, cpf));
+  }
+
+  @Override
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+
+  }
+
+  @Override
+  public void mostrarResultado() {
+
+  }
 }
--- src/main/java/com/betrybe/sistemadevotacao/Principal.java
+++ src/main/java/com/betrybe/sistemadevotacao/Principal.java
@@ -4,7 +4,11 @@ public class Principal {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
+  GerenciamentoVotacao teste = new GerenciamentoVotacao();
+  teste.cadastrarPessoaCandidata("Joao", 1);
+    System.out.println(teste.pessoasCandidatas);
 
+    teste.cadastrarPessoaCandidata("Maria", 1);
   }
 
 }
